d6e553f719465a450321921ac257ecd29d879bb2
Aamnah Akram <aamnah@aamnah.com>
2016-03-09 11:54:39 +0500

    -${REVISION} +${RASPI_MODEL}
    Removed $REVISION (/proc/cpuinfo) for a more precise and reliable $RASPI_MODEL (dmesg) command. No need to check every revision model in a conditional to find out which one is yours.

--- motd.sh
+++ motd.sh
@@ -4,7 +4,7 @@
 # IP=`ifconfig wlan0 | grep "inet addr" | cut -d ":" -f 2 | cut -d " " -f 1`
 IP=`hostname -I`
 EXT_IP=`wget -q -O - http://icanhazip.com/ | tail`
-REVISION=`cat /proc/cpuinfo | grep 'Revision' | awk '{print $3}' | sed 's/^1000//'`
+RASPI_MODEL=`dmesg | grep "Machine model:" | cut -d ":" -f 2`
 
 welcome() {
   echo "Bonjour!"
@@ -13,16 +13,7 @@ welcome() {
 # Identify Pi version
 # http://elinux.org/RPi_HardwareHistory
 version() {
-  if [ $REVISION == "000e" ]
-    then
-      echo "This a Raspberry Pi Model B - 512MB RAM. "
-  elif [ $REVISION == "0010" ]
-    then
-      echo "This is a Raspberry Pi Model B+ - 512MB RAM. "
-  elif [ $REVISION == "a21041" ]
-    then
-      echo "This is a Raspberry Pi 2 Model B - 1GB RAM. "
-  fi
+  echo "This is a" ${RASPI_MODEL}
 }
 
 # Show internal and external IP address
